c1880174ef8b677c66d96966472c5f66251f149d
SMG0000 <smg210743@gmail.com>
2022-11-30 23:40:11 +0900

    add Volume

---
+++ CustomScript/Volume/Volume.js
@@ -0,0 +1,18 @@
+//@version=1 //by SMG
+study("Volume")
+const 
+  color_total = input('Total',color.gray)
+header('')
+const 
+  switch_color = input('Switch Total Volume Color',false)
+const
+  color_vbuy = input("Buys",color.lime),
+  color_vsell = input("Sells",color.red)
+
+seq v = vbuy+vsell, d = vbuy-vsell
+seq color_v = d>0?color_vbuy:color_vsell
+seq color = switch_color? color_v : color_total
+
+histogram(v, color=color,transp=80)
+histogram(vbuy, base=0,color=color_vbuy, width=0.1, offset=0.1) 
+histogram(vsell, base=0,color=color_vsell, width=0.1, offset=-0.1) 
